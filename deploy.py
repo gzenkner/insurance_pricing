ea48b237fab42fa2f5f1a4cd2b9cd96ef11c4794
Gabriel Zenkner <gabriel.zenkner@gmail.com>
2023-10-19 16:42:24 +0100

    minor changes

--- deploy.py
+++ deploy.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 with open('xgboost_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
+# provides a route that responds to HTTP request methods (POST), the function will be called when a POST request is made to the '/predict' URL. e.g., 127.0.0.1.5000/predict
 @app.route('/predict', methods=['POST'])
 def predict():
+    # predict function will be executed when a post request is made to 127.0.0.1.5000/predict
+
+    # get the json data from HTTP REQUEST
     user_input = request.get_json()
     prediction = model.predict(np.array([user_input['age'], user_input['sex'], user_input['bmi'],
                                          user_input['children'], user_input['smoker'], user_input['region']]).reshape(1, -1))
@@ -17,6 +21,7 @@ def predict():
     response = {
         "prediction": prediction
     }
+    # returns a JSON response to the client that made the POST request
     return jsonify(response)
 
 if __name__ == '__main__':
